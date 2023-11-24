5ab63f89ef2f140391c8fe83712c32118d624a4e
verci-eth <ciao@sonolibero.io>
2023-11-24 10:32:09 +0100

    placeholders

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <p>sunrise time</p>
         <Clock />
+        <p>sunset time</p>
       </header>
     </div>
   );
