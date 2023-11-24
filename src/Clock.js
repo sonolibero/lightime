525d616fc1b4b2501010880a8b06252274d1d9e4
verci-eth <ciao@sonolibero.io>
2023-11-24 10:43:08 +0100

    two digits

--- src/Clock.js
+++ src/Clock.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
 function Clock() {
-  const [time, setTime] = useState(new Date());
+    const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 100); // update every 100 milliseconds
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 100); // update every 100 milliseconds
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-  return (
-    <p>
-      {`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}.${Math.floor(time.getMilliseconds() / 100)}`}
-    </p>
-  );
+    const formatTime = (timeElement) => timeElement.toString().padStart(2, '0');
+
+    return (
+        <p>
+            {`${formatTime(time.getHours())}:${formatTime(time.getMinutes())}:${formatTime(time.getSeconds())}${Math.floor(time.getMilliseconds() / 100)}`}
+        </p>
+    );
 }
 
 export default Clock;
