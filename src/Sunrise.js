fe4aabb7db206c784ed87a2cd215b548a07bd02c
verci-eth <ciao@sonolibero.io>
2023-11-24 11:27:37 +0100

    sunrise time

--- src/Sunrise.js
+++ src/Sunrise.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { getSunrise } from 'sunrise-sunset-js';
+
 function Sunrise() {
+    const [sunrise, setSunrise] = useState(null);
+
+    useEffect(() => {
+        const latitude = 40.7128; // replace with your latitude
+        const longitude = -74.0060; // replace with your longitude
+        const sunriseTime = getSunrise(latitude, longitude);
+
+        setSunrise(sunriseTime.toLocaleTimeString());
+    }, []);
+
     return (
-      <p>
-        sunrise time
-      </p>
+        <p>
+            {sunrise ? `Sunrise time: ${sunrise}` : 'Calculating sunrise time...'}
+        </p>
     );
-  }
-  
-  export default Sunrise;
+}
+
+export default Sunrise;
